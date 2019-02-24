9b964e716f49e9dc70447ecbf5c6a311bc0203c5
Unknown <pau.trepat.segura@gmail.com>
2019-02-24 20:00:49 +0100

    Update dependencies and setup packages

--- katas_java/01-mars-rover-movement/src/test/java/com/codesai/marsrover/movement/RoverTest.java
+++ katas_java/01-mars-rover-movement/src/test/java/com/codesai/marsrover/movement/RoverTest.java
@@ -1,12 +1,14 @@
-import org.junit.Test;
+package com.codesai.marsrover.movement;
+
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class RoverTest {
+class RoverTest {
 
     @Test
-    public void fix_and_rename_me() {
+    void fix_and_rename_me() {
         assertThat(true, is(false));
     }
 }
