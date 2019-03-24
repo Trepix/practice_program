bd0904b81a258875c58e2508bc468986eea98f67
Unknown <pau.trepat.segura@gmail.com>
2019-03-25 00:15:32 +0100

    Add Cardinal Direction creating for North

---
+++ katas_java/02_smelly-mars-rover-refactoring/src/main/java/mars_rover/CardinalDirection.java
@@ -0,0 +1,10 @@
+package mars_rover;
+
+public enum CardinalDirection {
+    NORTH, SOUTH, EAST, WEST;
+
+    public static CardinalDirection from(String cardinalDirection) {
+        if ("N".equals(cardinalDirection)) return NORTH;
+        return NORTH;
+    }
+}
---
+++ katas_java/02_smelly-mars-rover-refactoring/src/test/java/CardinalDirectionCreationTests.java
@@ -0,0 +1,12 @@
+import mars_rover.CardinalDirection;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CardinalDirectionCreationTests {
+
+    @Test
+    public void creatingNorth() {
+        assertThat(CardinalDirection.from("N"), is(CardinalDirection.NORTH));
+    }
+}
