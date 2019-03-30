d8f56ad07878b9a7811ba6b854daa1083fea527c
Unknown <pau.trepat.segura@gmail.com>
2019-03-30 11:29:07 +0100

    Inline static fields with no reuse

--- katas_java/03-mars-rover-adding-new-feature/initial-code/src/main/java/mars_rover/interpreters/NasaCommunicationInterpreter.java
+++ katas_java/03-mars-rover-adding-new-feature/initial-code/src/main/java/mars_rover/interpreters/NasaCommunicationInterpreter.java
@@ -11,11 +11,6 @@ import java.util.stream.Collectors;
 public class NASACommunicationInterpreter implements CommunicationInterpreter {
 
     private static final int MOVEMENT_DELTA = 1;
-    private static final NavigationCommand TURN_RIGHT_COMMAND = new TurnRightCommand();
-    private static final NavigationCommand TURN_LEFT_COMMAND = new TurnLeftCommand();
-    private static final NavigationCommand MOVE_FORWARD_COMMAND = new MoveForwardCommand(MOVEMENT_DELTA);
-    private static final NavigationCommand MOVE_BACKWARD_COMMAND = new MoveBackwardCommand(MOVEMENT_DELTA);
-    private static final NavigationCommand UNKNOWN_COMMAND = new NoOperationCommand();
 
     @Override
     public List<NavigationCommand> translateSequence(String commandSequence) {
@@ -29,15 +24,15 @@ public class NASACommunicationInterpreter implements CommunicationInterpreter {
     private NavigationCommand translate(String command) {
         switch (command) {
             case "r":
-                return TURN_RIGHT_COMMAND;
+                return new TurnRightCommand();
             case "l":
-                return TURN_LEFT_COMMAND;
+                return new TurnLeftCommand();
             case "f":
-                return MOVE_FORWARD_COMMAND;
+                return new MoveForwardCommand(MOVEMENT_DELTA);
             case "b":
-                return MOVE_BACKWARD_COMMAND;
+                return new MoveBackwardCommand(MOVEMENT_DELTA);
             default:
-                return UNKNOWN_COMMAND;
+                return new NoOperationCommand();
         }
     }
 }
