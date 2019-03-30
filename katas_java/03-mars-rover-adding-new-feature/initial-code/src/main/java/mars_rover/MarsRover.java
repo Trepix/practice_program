1b1de82f140b8e5f67e8419b5f82648ace7b37f5
Unknown <pau.trepat.segura@gmail.com>
2019-03-30 11:05:22 +0100

    Add as class field the communication interpreter

--- katas_java/03-mars-rover-adding-new-feature/initial-code/src/main/java/mars_rover/MarsRover.java
+++ katas_java/03-mars-rover-adding-new-feature/initial-code/src/main/java/mars_rover/MarsRover.java
@@ -6,14 +6,16 @@ import mars_rover.interpreters.NASACommunicationInterpreter;
 import mars_rover.navigation.Coordinates;
 import mars_rover.navigation.Direction;
 
-@ToString
-@EqualsAndHashCode
+@ToString(exclude = "communicationInterpreter")
+@EqualsAndHashCode(exclude = "communicationInterpreter")
 public class MarsRover {
 
     private Navigation navigation;
+    private CommunicationInterpreter communicationInterpreter;
 
     public MarsRover(int x, int y, String direction) {
         this.navigation = new Navigation(new Coordinates(x, y), Direction.parse(direction));
+        this.communicationInterpreter = new NASACommunicationInterpreter();
     }
 
     public void receive(String commandsSequence) {
@@ -27,6 +29,6 @@ public class MarsRover {
     }
 
     private NavigationCommand getCommand(String command) {
-        return new NASACommunicationInterpreter().translate(command);
+        return this.communicationInterpreter.translate(command);
     }
 }
