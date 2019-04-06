a4b9bc4d5a90dccbaef94370adc0b9328b85203d
Unknown <pau.trepat.segura@gmail.com>
2019-04-06 20:21:42 +0200

    Add turn right command for nasa interpretation on multi interpreter Mars Rover

--- katas_java/03-mars-rover-adding-new-feature/initial-code/src/main/java/mars_rover/interpreters/MultipleCommunicationInterpreter.java
+++ katas_java/03-mars-rover-adding-new-feature/initial-code/src/main/java/mars_rover/interpreters/MultipleCommunicationInterpreter.java
@@ -2,16 +2,40 @@ package mars_rover.interpreters;
 
 import mars_rover.CommunicationInterpreter;
 import mars_rover.NavigationCommand;
-import mars_rover.commands.NoOperationCommand;
+import mars_rover.commands.*;
 
-import java.util.List;
+import java.util.*;
 
 import static java.util.Collections.singletonList;
 
+
 public class MultipleCommunicationInterpreter implements CommunicationInterpreter {
+
+    private CommunicationInterpreter currentInterpreter = null;
 
     @Override
     public List<NavigationCommand> translateSequence(String commandSequence) {
+        List<NavigationCommand> result = new LinkedList<>();
+        for (String command : split(commandSequence)){
+            result.addAll(translate(command));
+        }
+        return result;
+    }
+
+    private List<String> split(String commandSequence) {
+        return Arrays.asList(commandSequence.split(""));
+    }
+
+    private List<NavigationCommand> translate(String command) {
+        if ("z".equals(command)) {
+            currentInterpreter = new NASACommunicationInterpreter();
+        } else if (getInterpreter().isPresent()) {
+            return getInterpreter().get().translateSequence(command);
+        }
         return singletonList(new NoOperationCommand());
+    }
+
+    private Optional<CommunicationInterpreter> getInterpreter() {
+        return Optional.ofNullable(currentInterpreter);
     }
 }
--- katas_java/03-mars-rover-adding-new-feature/initial-code/src/test/java/mars_rover/MultipleCommunicationMarsRoverTest.java
+++ katas_java/03-mars-rover-adding-new-feature/initial-code/src/test/java/mars_rover/MultipleCommunicationMarsRoverTest.java
@@ -18,12 +18,47 @@ public class MultipleCommunicationMarsRoverTest {
     }
 
     @Test
-    public void does_nothing_when_communication_interpreter_is_not_specified() {
+    public void does_nothing_when_commands_interpreter_is_not_specified() {
         MarsRover marsRover = anyMultiInterpreterMarsRover().build();
 
         marsRover.receive("rlbx");
 
         assertThat(marsRover, is(anyMultiInterpreterMarsRover().build()));
+    }
+
+    @Test
+    public void turns_right_when_pointing_north_using_nasa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("N").build();
+
+        marsRover.receive("zr");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("E").build()));
+    }
+    @Test
+    public void turns_right_when_pointing_east_using_nasa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("E").build();
+
+        marsRover.receive("zr");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("S").build()));
+    }
+
+    @Test
+    public void turns_right_when_pointing_south_using_nasa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("S").build();
+
+        marsRover.receive("zr");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("W").build()));
+    }
+
+    @Test
+    public void turns_right_when_pointing_west_using_nasa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("W").build();
+
+        marsRover.receive("zr");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("N").build()));
     }
 
 }
