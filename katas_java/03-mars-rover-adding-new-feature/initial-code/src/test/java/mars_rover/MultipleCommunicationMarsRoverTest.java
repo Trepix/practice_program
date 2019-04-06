0448d155279947eab6066abd06196ba09ab22c5f
Unknown <pau.trepat.segura@gmail.com>
2019-04-06 20:50:08 +0200

    Add the rest of ESA commands test for multi interpreter Mars Rover

--- katas_java/03-mars-rover-adding-new-feature/initial-code/src/test/java/mars_rover/MultipleCommunicationMarsRoverTest.java
+++ katas_java/03-mars-rover-adding-new-feature/initial-code/src/test/java/mars_rover/MultipleCommunicationMarsRoverTest.java
@@ -8,6 +8,7 @@ import static org.hamcrest.core.Is.is;
 
 public class MultipleCommunicationMarsRoverTest {
 
+
     @Test
     public void does_nothing_when_receiving_empty_commands_sequence() {
         MarsRover marsRover = anyMultiInterpreterMarsRover().build();
@@ -25,6 +26,8 @@ public class MultipleCommunicationMarsRoverTest {
 
         assertThat(marsRover, is(anyMultiInterpreterMarsRover().build()));
     }
+
+    /*NASA commands*/
 
     @Test
     public void turns_right_when_pointing_north_using_nasa_commands() {
@@ -178,6 +181,8 @@ public class MultipleCommunicationMarsRoverTest {
         assertThat(marsRover, is(aMultiInterpreterMarsRover().at(8, 4).facing("S").build()));
     }
 
+    /*ESA commands*/
+
     @Test
     public void turns_right_when_pointing_north_using_esa_commands() {
         MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("N").build();
@@ -212,6 +217,123 @@ public class MultipleCommunicationMarsRoverTest {
         marsRover.receive("kl");
 
         assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("N").build()));
+    }
+
+    @Test
+    public void turns_left_when_pointing_north_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("N").build();
+
+        marsRover.receive("kf");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("W").build()));
+    }
+
+    @Test
+    public void turns_left_when_pointing_west_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("W").build();
+
+        marsRover.receive("kf");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("S").build()));
+    }
+
+    @Test
+    public void turns_left_when_pointing_south_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("S").build();
+
+        marsRover.receive("kf");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("E").build()));
+    }
+
+    @Test
+    public void turns_left_when_pointing_east_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRoverAnywhere().facing("E").build();
+
+        marsRover.receive("kf");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRoverAnywhere().facing("N").build()));
+    }
+
+    @Test
+    public void moves_forward_when_pointing_north_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("N").build();
+
+        marsRover.receive("kb");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(5, 5).facing("N").build()));
+    }
+
+    @Test
+    public void moves_forward_when_pointing_east_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("E").build();
+
+        marsRover.receive("kb");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(6, 4).facing("E").build()));
+    }
+
+    @Test
+    public void moves_forward_when_pointing_south_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("S").build();
+
+        marsRover.receive("kb");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(5, 3).facing("S").build()));
+    }
+
+    @Test
+    public void moves_forward_when_pointing_west_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("W").build();
+
+        marsRover.receive("kb");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(4, 4).facing("W").build()));
+    }
+
+    @Test
+    public void moves_backward_when_pointing_north_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("N").build();
+
+        marsRover.receive("kx");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(5, 3).facing("N").build()));
+    }
+
+    @Test
+    public void moves_backward_when_pointing_east_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("E").build();
+
+        marsRover.receive("kx");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(4, 4).facing("E").build()));
+    }
+
+    @Test
+    public void moves_backward_when_pointing_south_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("S").build();
+
+        marsRover.receive("kx");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(5, 5).facing("S").build()));
+    }
+
+    @Test
+    public void moves_backward_when_pointing_west_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(5, 4).facing("W").build();
+
+        marsRover.receive("kx");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(6, 4).facing("W").build()));
+    }
+
+    @Test
+    public void receives_multiple_commands_using_esa_commands() {
+        MarsRover marsRover = aMultiInterpreterMarsRover().at(7, 4).facing("E").build();
+
+        marsRover.receive("kbl");
+
+        assertThat(marsRover, is(aMultiInterpreterMarsRover().at(8, 4).facing("S").build()));
     }
 
 }
