314c9ccb41ee2a40ca1952467df5b8608d3e1c04
Trepix <pau.trepat.segura@gmail.com>
2019-05-20 10:34:58 +0200

    Add Beverage to class and create constructor on CoffeeWithMilkAndCream

--- katas_java/05-refactoring-awful-inheritance-use-with-beverage-prices-kata/src/main/java/beverages/CoffeeWithMilkAndCream.java
+++ katas_java/05-refactoring-awful-inheritance-use-with-beverage-prices-kata/src/main/java/beverages/CoffeeWithMilkAndCream.java
@@ -1,6 +1,17 @@
 package beverages;
 
 public class CoffeeWithMilkAndCream extends Coffee {
+
+    private final Beverage beverage;
+
+    public CoffeeWithMilkAndCream() {
+        this.beverage = null;
+    }
+
+    public CoffeeWithMilkAndCream(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
     @Override
     public double price() {
         return super.price() +  0.25;
