3aee0fc502cfe5c58faff5eb974cfa6853f2bd36
Trepix <pau.trepat.segura@gmail.com>
2019-06-02 02:03:50 +0200

    Change repository implementation to use new ourdate constructor. Step 2

--- katas_java/06-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/infrastructure/FileEmployeeRepository.java
+++ katas_java/06-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/infrastructure/FileEmployeeRepository.java
@@ -8,6 +8,7 @@ import birthdaygreetings.OurDate;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -30,7 +31,7 @@ public class FileEmployeeRepository implements EmployeeRepository {
             while ((str = in.readLine()) != null) {
                 String[] employeeData = str.split(", ");
                 Employee employee = new Employee(employeeData[1], employeeData[0],
-                        new OurDate(employeeData[2]), employeeData[3]);
+                        new OurDate(new SimpleDateFormat("yyyy/MM/dd").parse(employeeData[2])), employeeData[3]);
                 if (employee.isBirthday(ourDate)) {
                     employeesThatIsHisBirthday.add(employee);
                 }
