7663c5812fb8f5d2093e0e6e813d9845dac14385
Trepix <pau.trepat.segura@gmail.com>
2019-06-02 02:15:18 +0200

    Change OurDateTest to use new constructor of OurDate. Step 3

---
+++ katas_java/06-refactoring-to-hexagonal-architecture/src/test/java/birthdaygreetings/test/OurDateFactory.java
@@ -0,0 +1,13 @@
+package birthdaygreetings.test;
+
+import birthdaygreetings.OurDate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class OurDateFactory {
+
+    public static OurDate create(String date) throws ParseException {
+        return new OurDate(new SimpleDateFormat("yyyy/MM/dd").parse(date));
+    }
+}
