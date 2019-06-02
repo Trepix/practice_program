6adcc5950992503864bbd31ccd0bce95a858d4e4
Trepix <pau.trepat.segura@gmail.com>
2019-06-03 00:03:27 +0200

    Extract to constants magic numbers of main

--- katas_java/06-refactoring-to-hexagonal-architecture/src/main/java/cli/Main.java
+++ katas_java/06-refactoring-to-hexagonal-architecture/src/main/java/cli/Main.java
@@ -4,12 +4,19 @@ import birthdaygreetings.BirthdayService;
 import birthdaygreetings.infrastructure.FileEmployeeRepository;
 import birthdaygreetings.infrastructure.OurDateFactory;
 
+import static birthdaygreetings.infrastructure.OurDateFactory.*;
+
 public class Main {
+
+    private static final String DATE = "2008/10/08";
+    private static final String SMTP_HOST = "localhost";
+    private static final int SMTP_PORT = 25;
+    private static final String FILE_WITH_EMPLOYEES = "employee_data.txt";
+
     public static void main(String[] args) {
-        BirthdayService service = new BirthdayService(new FileEmployeeRepository("employee_data.txt"));
+        BirthdayService service = new BirthdayService(new FileEmployeeRepository(FILE_WITH_EMPLOYEES));
         try {
-            service.sendGreetings(
-                    OurDateFactory.createFromDateSeparatedBySlash("2008/10/08"), "localhost", 25);
+            service.sendGreetings(createFromDateSeparatedBySlash(DATE), SMTP_HOST, SMTP_PORT);
         } catch (Exception e) {
             e.printStackTrace();
         }
