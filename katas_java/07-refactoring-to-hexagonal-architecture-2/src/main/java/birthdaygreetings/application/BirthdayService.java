937948acce84114539debb7fd20957fb9cd96f76
Trepix <pau.trepat.segura@gmail.com>
2019-06-16 20:15:40 +0200

    Push to Greetings Sender  action of construct the message. Step 2

--- katas_java/07-refactoring-to-hexagonal-architecture-2/src/main/java/birthdaygreetings/application/BirthdayService.java
+++ katas_java/07-refactoring-to-hexagonal-architecture-2/src/main/java/birthdaygreetings/application/BirthdayService.java
@@ -43,29 +43,10 @@ public class BirthdayService {
             String recipient = message.to();
             String body = message.text();
             String subject = message.subject();
-            sendMessage(smtpHost, smtpPort, sender, subject, body, recipient);
+            greetingsSender.sendMessage(smtpHost, smtpPort, sender, subject, body, recipient);
         }
     }
 
-    private void sendMessage(String smtpHost, int smtpPort, String sender,
-                             String subject, String body, String recipient)
-        throws MessagingException {
-        // Create a mail session
-        java.util.Properties props = new java.util.Properties();
-        props.put("mail.smtp.host", smtpHost);
-        props.put("mail.smtp.port", "" + smtpPort);
-        Session session = Session.getDefaultInstance(props, null);
 
-        // Construct the message
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress(sender));
-        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(
-            recipient));
-        msg.setSubject(subject);
-        msg.setText(body);
-
-        // Send the message
-        this.greetingsSender.sendMessage(msg);
-    }
 
 }
--- katas_java/07-refactoring-to-hexagonal-architecture-2/src/main/java/birthdaygreetings/infrastructure/senders/GreetingsSender.java
+++ katas_java/07-refactoring-to-hexagonal-architecture-2/src/main/java/birthdaygreetings/infrastructure/senders/GreetingsSender.java
@@ -2,9 +2,33 @@ package birthdaygreetings.infrastructure.senders;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class GreetingsSender {
+
+    public void sendMessage(String smtpHost, int smtpPort, String sender,
+                             String subject, String body, String recipient)
+            throws MessagingException {
+        // Create a mail session
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "" + smtpPort);
+        Session session = Session.getDefaultInstance(props, null);
+
+        // Construct the message
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(sender));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(
+                recipient));
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        // Send the message
+        this.sendMessage(msg);
+    }
 
     public void sendMessage(Message msg) throws MessagingException {
         Transport.send(msg);
