6e4039e9c616de3af9e149ac76e040246bcc24af
Trepix <pau.trepat.segura@gmail.com>
2019-09-30 00:52:26 +0200

    Searching edge cases. Covering last hour of the morning

--- katas_java/09-outside-in-with-ohce-kata/part_2/src/test/java/ohce/OcheTest.java
+++ katas_java/09-outside-in-with-ohce-kata/part_2/src/test/java/ohce/OcheTest.java
@@ -47,6 +47,26 @@ public class OcheTest {
     }
 
     @Test
+    public void runs_when_is_in_the_last_hour_of_the_morning_receiving_palindrome_and_non_palindrome_words() {
+        Reader reader = mock(Reader.class);
+        Writer writer = mock(Writer.class);
+        Hour hour = new StubHour(11);
+        Oche oche = new Oche(reader, writer, hour);
+        when(reader.nextWord()).thenReturn("hola", "oto", "stop", "Stop!");
+
+        oche.runs("Karl");
+
+        verifyThatHasBeenWritten(writer,
+                "¡Buenas días Karl!",
+                "aloh",
+                "oto",
+                "¡Bonita palabra!",
+                "pots",
+                "Adios Karl");
+    }
+
+
+    @Test
     public void runs_when_is_in_the_afternoon_receiving_palindrome_and_non_palindrome_words() {
         Reader reader = mock(Reader.class);
         Writer writer = mock(Writer.class);
