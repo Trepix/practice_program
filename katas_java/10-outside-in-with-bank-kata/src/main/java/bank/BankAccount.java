f17f171a65489d12bbce2e6c610e08328a787605
Trepix <pau.trepat.segura@gmail.com>
2019-12-05 20:49:04 +0100

    Finish Bank Account to make acceptance test pass

--- katas_java/10-outside-in-with-bank-kata/src/main/java/bank/BankAccount.java
+++ katas_java/10-outside-in-with-bank-kata/src/main/java/bank/BankAccount.java
@@ -1,16 +1,28 @@
 package bank;
 
+import bank.statement.Statement;
+import bank.statement.StatementPrinter;
+
 public class BankAccount {
 
+    private final Calendar calendar;
+    private final Statement statement = new Statement();
+    private final StatementPrinter statementPrinter;
+
     public BankAccount(Calendar calendar, Display display) {
+        this.calendar = calendar;
+        this.statementPrinter = new StatementPrinter(display);
     }
 
     public void deposit(int amount) {
+        statement.add(BankingTransaction.deposit(calendar.date(), amount));
     }
 
     public void withdraw(int amount) {
+        statement.add(BankingTransaction.withdrawal(calendar.date(), amount));
     }
 
     public void printStatement() {
+        statementPrinter.print(statement);
     }
 }
