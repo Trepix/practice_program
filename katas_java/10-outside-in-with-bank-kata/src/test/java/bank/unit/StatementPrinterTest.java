2dfdf4738ffb9880ec36e6c44ac38302aae4059e
Trepix <pau.trepat.segura@gmail.com>
2019-12-05 20:24:34 +0100

    Print a statement with multiple banking transactions

--- katas_java/10-outside-in-with-bank-kata/src/test/java/bank/unit/StatementPrinterTest.java
+++ katas_java/10-outside-in-with-bank-kata/src/test/java/bank/unit/StatementPrinterTest.java
@@ -69,6 +69,22 @@ public class StatementPrinterTest {
     }
 
     @Test
+    public void statement_with_deposits_and_withdrawals() {
+        Display display = Mockito.spy(Display.class);
+        StatementPrinter printer = new StatementPrinter(display);
+        Statement statement = new Statement();
+        statement.add(BankingTransaction.deposit(DateUtils.parse("10/01/2012"), 1000));
+        statement.add(BankingTransaction.withdrawal(DateUtils.parse("11/01/2012"), 1500));
+
+        printer.print(statement);
+
+        InOrder statementOrder = Mockito.inOrder(display);
+        statementOrder.verify(display).show("date || credit || debit || balance");
+        statementOrder.verify(display).show("11/01/2012 || || 1500.00 || -500.00");
+        statementOrder.verify(display).show("10/01/2012 || 1000.00 || || 1000.00");
+    }
+
+    @Test
     @Ignore
     public void when_statement_is_a_deposit_and_withdrawal_is_called() {
         BankingTransaction transaction = BankingTransaction.deposit(DateUtils.parse("10/01/2012"), 1000);
