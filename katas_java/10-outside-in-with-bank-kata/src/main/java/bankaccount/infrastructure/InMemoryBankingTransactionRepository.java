35a7fc12511cd578bae6cb512a206609570bb102
Trepix <pau.trepat.segura@gmail.com>
2020-01-13 00:23:33 +0100

    WIP: InMemoryRepository generates a statement when a deposit is saved

--- katas_java/10-outside-in-with-bank-kata/src/main/java/bankaccount/infrastructure/InMemoryBankingTransactionRepository.java
+++ katas_java/10-outside-in-with-bank-kata/src/main/java/bankaccount/infrastructure/InMemoryBankingTransactionRepository.java
@@ -4,14 +4,22 @@ import bankaccount.domain.bankingtransactions.BankingTransaction;
 import bankaccount.domain.bankingtransactions.BankingTransactionRepository;
 import bankaccount.domain.statement.Statement;
 
+import java.util.LinkedList;
+
 public class InMemoryBankingTransactionRepository implements BankingTransactionRepository {
+
+    private LinkedList<BankingTransaction> bankingTransactions = new LinkedList<>();
+
     @Override
     public void add(BankingTransaction bankingTransaction) {
-
+        bankingTransactions.push(bankingTransaction);
     }
 
     @Override
     public Statement generateStatement() {
-        return new Statement();
+        if (bankingTransactions.isEmpty()) return new Statement();
+        Statement statement = new Statement();
+        statement.add(bankingTransactions.get(0));
+        return statement;
     }
 }
--- katas_java/10-outside-in-with-bank-kata/src/test/java/bankaccount/unit/InMemoryBankingTransactionRepositoryTest.java
+++ katas_java/10-outside-in-with-bank-kata/src/test/java/bankaccount/unit/InMemoryBankingTransactionRepositoryTest.java
@@ -1,5 +1,8 @@
 package bankaccount.unit;
 
+import bankaccount.DateHelper;
+import bankaccount.StatementBuilder;
+import bankaccount.domain.bankingtransactions.BankingTransaction;
 import bankaccount.domain.bankingtransactions.BankingTransactionRepository;
 import bankaccount.domain.statement.Statement;
 import bankaccount.infrastructure.InMemoryBankingTransactionRepository;
@@ -7,6 +10,10 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static bankaccount.DateHelper.*;
+import static bankaccount.StatementBuilder.*;
+import static bankaccount.domain.bankingtransactions.BankingTransaction.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class InMemoryBankingTransactionRepositoryTest {
@@ -24,7 +31,16 @@ public class InMemoryBankingTransactionRepositoryTest {
 
         Statement statement = bankingTransactionRepository.generateStatement();
 
-        assertThat(statement, Matchers.is(emptyStatement));
+        assertThat(statement, is(emptyStatement));
+    }
+
+    @Test
+    public void returns_statement_with_deposit() {
+        BankingTransaction deposit = deposit(date("02/02/1943"), 1000);
+        Statement statement = with(deposit).build();
+
+        bankingTransactionRepository.add(deposit);
+        assertThat(bankingTransactionRepository.generateStatement(), is(statement));
     }
 
 }
