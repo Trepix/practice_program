35c5739e121e8cca185cafff504e09a296b5f4d3
Trepix <pau.trepat.segura@gmail.com>
2020-02-15 20:35:07 +0100

    WIP: Test when there are no payments

--- katas_java/11-outside-in-with-unusual-spending-alert-service/src/main/java/alert_service/detection/Detector.java
+++ katas_java/11-outside-in-with-unusual-spending-alert-service/src/main/java/alert_service/detection/Detector.java
@@ -3,8 +3,16 @@ package alert_service.detection;
 import alert_service.UnusualExpenses;
 import alert_service.notify.UserId;
 
+import java.util.Collections;
+
+import static java.util.Collections.*;
+
 public class Detector {
+    public Detector(PaymentsRepository paymentsRepository) {
+
+    }
+
     public UnusualExpenses detect(UserId userId) {
-        return null;
+        return new UnusualExpenses(userId, emptyList());
     }
 }
--- katas_java/11-outside-in-with-unusual-spending-alert-service/src/test/java/alert_service/unit/detector/DetectorTest.java
+++ katas_java/11-outside-in-with-unusual-spending-alert-service/src/test/java/alert_service/unit/detector/DetectorTest.java
@@ -1,14 +1,41 @@
 package alert_service.unit.detector;
 
+import alert_service.UnusualExpenses;
+import alert_service.detection.Detector;
+import alert_service.detection.Payments;
+import alert_service.detection.PaymentsRepository;
 import alert_service.notify.UserId;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DetectorTest {
 
     private final UserId userId = UserId.of("1234");
+    private PaymentsRepository paymentsRepository;
+    private Detector detector;
+
+    @Before
+    public void setUp() {
+       this.paymentsRepository = mock(PaymentsRepository.class);
+       this.detector = new Detector(paymentsRepository);
+    }
 
     @Test
     public void when_there_are_no_payments_should_return_empty_unusual_expenses() {
+        UnusualExpenses emptyUnusualExpenses = new UnusualExpenses(userId, emptyList());
+        Payments noPayments = new Payments(userId, emptyList());
+        when(paymentsRepository.getByUserId(userId)).thenReturn(noPayments);
 
+        UnusualExpenses unusualExpenses = detector.detect(userId);
+
+        assertThat(unusualExpenses, is(emptyUnusualExpenses));
     }
 }
