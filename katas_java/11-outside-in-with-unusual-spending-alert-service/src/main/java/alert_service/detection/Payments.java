5a4710114cd7baa809a16915dc328fdf52f15c23
Trepix <pau.trepat.segura@gmail.com>
2020-02-23 17:07:16 +0100

    WIP: Refactoring adding semantics and splitting responsabilities

--- katas_java/11-outside-in-with-unusual-spending-alert-service/src/main/java/alert_service/detection/Payments.java
+++ katas_java/11-outside-in-with-unusual-spending-alert-service/src/main/java/alert_service/detection/Payments.java
@@ -5,14 +5,12 @@ import alert_service.UnusualExpenses;
 import alert_service.notify.UserId;
 import lombok.RequiredArgsConstructor;
 
-import javax.swing.text.html.CSS;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
-import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
@@ -26,25 +24,27 @@ public class Payments {
     }
 
     public UnusualExpenses findUnusual(LocalDate today) {
-        List<Payment> currentMonthPayments = currentMonthPayments(today);
-        List<Payment> monthBeforePayments = monthBeforePayments(today);
-        if (currentMonthPayments.isEmpty()) return new UnusualExpenses(userId, emptyList());
-        List<CategorySpending> currentMonthCategorySpending = sumUnusualExpenses(currentMonthPayments);
-        List<CategorySpending> monthBeforeCategorySpending = sumUnusualExpenses(monthBeforePayments);
-        List<UnusualExpense> unusualExpensesDetected = new LinkedList<>();
-        for (CategorySpending categoryExpenseMonthBefore : monthBeforeCategorySpending) {
-            Optional<CategorySpending> currentMonthCategoryExpense = currentMonthCategorySpending.stream().filter(x -> x.category.equals(categoryExpenseMonthBefore.category)).findFirst();
-            currentMonthCategoryExpense.filter(x -> categoryExpenseMonthBefore.amount*1.5 < x.amount).map(x -> new UnusualExpense(x.category, x.amount)).ifPresent(unusualExpensesDetected::add);
-        }
+        List<CategorySpending> currentMonthSpending = sumCategoryExpensesOfCurrentMonth(today);
+        List<CategorySpending> monthBeforeSpending = sumCategoryExpensesOfMonthBefore(today);
+        List<UnusualExpense> unusualExpensesDetected = detectUnusual(currentMonthSpending, monthBeforeSpending);
         return new UnusualExpenses(userId, unusualExpensesDetected);
     }
 
-    private List<Payment> monthBeforePayments(LocalDate today) {
-        return filterByMonth(today.getMonth().minus(1));
+    private List<UnusualExpense> detectUnusual(List<CategorySpending> currentMonth, List<CategorySpending> monthBefore) {
+        List<UnusualExpense> unusualExpensesDetected = new LinkedList<>();
+        for (CategorySpending categoryExpenseMonthBefore : monthBefore) {
+            Optional<CategorySpending> currentMonthCategoryExpense = currentMonth.stream().filter(x -> x.category.equals(categoryExpenseMonthBefore.category)).findFirst();
+            currentMonthCategoryExpense.filter(x -> categoryExpenseMonthBefore.amount * 1.5 < x.amount).map(x -> new UnusualExpense(x.category, x.amount)).ifPresent(unusualExpensesDetected::add);
+        }
+        return unusualExpensesDetected;
     }
 
-    private List<Payment> currentMonthPayments(LocalDate today) {
-        return filterByMonth(today.getMonth());
+    private List<CategorySpending> sumCategoryExpensesOfCurrentMonth(LocalDate today) {
+        return sumCategoryExpenses(filterByMonth(today.getMonth()));
+    }
+
+    private List<CategorySpending> sumCategoryExpensesOfMonthBefore(LocalDate today) {
+        return sumCategoryExpenses(filterByMonth(today.getMonth().minus(1)));
     }
 
     private List<Payment> filterByMonth(Month month) {
@@ -53,7 +53,7 @@ public class Payments {
                 .collect(toList());
     }
 
-    private List<CategorySpending> sumUnusualExpenses(List<Payment> payments) {
+    private List<CategorySpending> sumCategoryExpenses(List<Payment> payments) {
         return payments.stream()
                 .collect(groupingBy(Payment::category))
                 .entrySet().stream()
