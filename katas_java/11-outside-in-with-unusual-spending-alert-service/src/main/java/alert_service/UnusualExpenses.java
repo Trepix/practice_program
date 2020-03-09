fe2624ecb9dd261069ff6e87eb9ab4ccf5c1f4c6
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2020-03-09 16:48:49 +0100

    Fix inconsistency between equals and hashcode. Use Set in both

--- katas_java/11-outside-in-with-unusual-spending-alert-service/src/main/java/alert_service/UnusualExpenses.java
+++ katas_java/11-outside-in-with-unusual-spending-alert-service/src/main/java/alert_service/UnusualExpenses.java
@@ -3,10 +3,7 @@ package alert_service;
 import alert_service.notify.UserId;
 import lombok.ToString;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @ToString
 public class UnusualExpenses implements Iterable<UnusualExpense> {
@@ -41,18 +38,12 @@ public class UnusualExpenses implements Iterable<UnusualExpense> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UnusualExpenses that = (UnusualExpenses) o;
-        return Objects.equals(userId, that.userId) &&
-                ((unusualExpenses == that.unusualExpenses) ||
-                        (unusualExpenses != null && unusualExpenses.containsAll(that.unusualExpenses) && that.unusualExpenses.containsAll(unusualExpenses)));
+        return Objects.equals(userId, that.userId)
+                && Objects.equals(new HashSet<>(this.unusualExpenses), new HashSet<>(that.unusualExpenses));
     }
-
-    /*
-    TODO: never rewrite equals without do the same with hashcode
-    If two objects are equal according to the equals(Object) method,
-    then calling the hashcode() method on each of the two objects must produce the same integer result.
-    */
+    
     @Override
     public int hashCode() {
-        return Objects.hash(userId, unusualExpenses);
+        return Objects.hash(userId, new HashSet<>(this.unusualExpenses));
     }
 }
